cd21a03a7c5f96d2652528a7afcac95fb83001a2
MajorDown <romain.fouillaron@gmx.fr>
2023-12-16 21:52:07 +0100

    mise en forme du header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import AppLink from './AppLink';
 
 const Header = () => {
+  const mail = "romain.fouillaron@gmx.fr";
+  const points = "6";
+
   return (
-    <header>Header</header>
+    <header>
+      <div id="title">
+        <h1>Guilder</h1>
+        <img src="" alt="logo" />
+      </div>
+      <div id="headerNav">
+        <div id="userOptions">
+          <p>{mail}</p>
+          <p>|</p>
+          <p>{points} points</p>
+          <p>|</p>
+          <AppLink>Déconnexion</AppLink>
+        </div>
+        <nav id="operationNav">operationNav</nav>
+      </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
