a28d32aacd9b0b23809add4f0992ebc1691af8ae
MajorDown <romain.fouillaron@gmx.fr>
2024-01-01 23:33:37 +0100

    tentative d'intégration de Suspense et lazt

---
+++ app/guilde/loading.js
@@ -0,0 +1,6 @@
+import LoadSpinner from "@/components/LoadSpinner";
+
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return <LoadSpinner />;
+}
--- app/guilde/page.tsx
+++ app/guilde/page.tsx
@@ -1,50 +1,44 @@
 'use client'
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense, lazy } from "react";
 import { useUserContext } from "@/contexts/userContext";
 import AppLink from "@/components/AppLink";
 import { getGuildMembers } from "@/tools/front/getGuildMembers";
 import { MembersList } from "@/types";
-import MembersLister from "@/components/MembersLister";
+const MembersLister = lazy(() => import("@/components/MembersLister"));
 import LoadSpinner from "@/components/LoadSpinner";
 
 const Guilde = () => {
     const {user, updateUser} = useUserContext();
     const [members, setMembers] = useState<MembersList | []>();
-    const [isLoading, setIsLoading] = useState<boolean>(true);
     const [loadError, setLoadError] = useState<string>();
 
     useEffect(() => {
-        setIsLoading(true);
         const fetchMembers = async () => {
           if (user) {
             try {
               const guildMembers = await getGuildMembers(user.guild, user.token);
               setMembers(guildMembers);
-              setIsLoading(false);
             } catch (error) {
               setLoadError("erreur lors du chargement des membres de la guilde");
             }
           }
         };      
         fetchMembers();
-        setIsLoading(false);
       }, [user]);
 
-  return (
-    <section id="guildSection">
-        <h2>Membres de la guilde</h2>
-        {isLoading && <LoadSpinner />}
-        {loadError && <p>Une erreur est survenue lors du chargement des membres : {loadError}</p>}
-        {!isLoading && !user && <>
-            <p>Vous devez être connecté pour accéder à cette page.</p>
-            <AppLink href="/connexion" showActivation>Connexion</AppLink>
-        </>}
-        {user && members && <>
-        <p>liste des membres de {user.guild} :</p>
-        <MembersLister members={members} />
-        </>}
-    </section>
-  )
+  if(user) { 
+      return (
+          <section id="guildSection">
+              <h2>Membres de la guilde</h2>
+              <Suspense fallback={<LoadSpinner />}>
+                  {user && members && <>
+                      <p>liste des membres de {user.guild} :</p>
+                      <MembersLister members={members} />
+                  </>}
+                  {loadError && <p>Une erreur est survenue lors du chargement des membres : {loadError}</p>}
+              </Suspense>
+          </section>
+  )}
 }
 
 export default Guilde;
