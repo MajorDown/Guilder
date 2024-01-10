8a06801e28b490aee1751b57cea24cbdf81fe12a
MajorDown <romain.fouillaron@gmx.fr>
2024-01-11 00:29:56 +0100

    composant TextInput établie

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useState } from 'react';
 
-const TextInput = () => {
-  return (
-    <div>TextInput</div>
-  )
+// Définition des types pour les props
+interface Condition {
+    regex: RegExp;
+    error: string;
 }
 
-export default TextInput
+interface TextInputProps {
+    className?: string,
+    id?: string,
+    conditions: Condition[];
+    isValid: (valid: boolean) => void;
+}
+
+const TextInput = (props: TextInputProps) => {
+    const [value, setValue] = useState<string>('');
+    const [error, setError] = useState<string>('');
+
+    const handleBlur = () => {
+        let foundError: string = '';
+        props.conditions.forEach(condition => {
+            if (!condition.regex.test(value)) {
+                foundError = condition.error;
+            }
+        });
+        setError(foundError);
+        props.isValid(!foundError);
+    };
+    
+    return (
+        <div 
+            className={`textinput ${props.className}`}
+            id={props.id}
+        >
+            <input 
+                value={value} 
+                onChange={(event) => setValue(event.target.value)}
+                onBlur={() => handleBlur()}
+            />
+            {error && <p>{error}</p>}
+        </div>
+  );
+}
+
+export default TextInput;
