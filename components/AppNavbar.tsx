9c8056bfc87bc6eaf3f6c98d9682906f22eaff6c
MajorDown <romain.fouillaron@gmx.fr>
2024-02-06 22:54:08 +0100

    route api/member.login opérationnelle

--- components/AppNavbar.tsx
+++ components/AppNavbar.tsx
@@ -49,7 +49,6 @@ const AppNavbar = () => {
             {admin && (<>
                 <UINavLink label={"Membres"} href={'/membres'} icon={'/images/guild.svg'} showActivation/>
                 <UINavLink label={"Outils"} href={'/config'} icon={'/images/tools.svg'} showActivation/>
-                <UINavLink label={"Notifs"} href={'/'} icon={'/images/analyse.svg'} showActivation/>
                 <UINavLink label={"Options"} href={'/options'} icon={'/images/options.svg'} showActivation/>
                 <UINavLink label={"Déconnexion"} href={'/'} icon={'/images/logout.svg'} onClick={() => disconnectAdmin()} />
             </>
