d2f9ad828ec8c6c54e321446a468e8f9309c3fd5
MajorDown <romain.fouillaron@gmx.fr>
2024-04-25 00:47:43 +0200

    ajout de la possibilité de tri pour l'historique

--- components/InterventionsLister.tsx
+++ components/InterventionsLister.tsx
@@ -17,6 +17,8 @@ export type InterventionsListerProps = {
  */
 const InterventionsLister = (props: InterventionsListerProps) => {
     const [interventionsList, setInterventionsList] = useState<MemberInterventions>();
+    const [sortMethod, setSortMethod] = useState<"byInterventionDate" | "byDeclarationDate">("byInterventionDate");
+    const [sortedInterventionsList, setSortedInterventionsList] = useState<MemberInterventions>();
   
     useEffect(() => {
         if (props.user) {
@@ -32,13 +34,45 @@ const InterventionsLister = (props: InterventionsListerProps) => {
           };
           getInterventions();
         }
-  }, [props.user]);
+    }, [props.user]);
+
+//format de declarationDate : "2024-04-22-11-32-10-246"
+//format de interventionDate : "2024-04-22"
+
+useEffect(() => {
+  if (interventionsList) {
+    let sortedInterventions;
+    switch (sortMethod) {
+      case "byDeclarationDate":
+        sortedInterventions = [...interventionsList].sort((a, b) => {
+          // Convertit les dates de déclaration en grands nombres entiers pour les comparer
+          const numA = parseInt(a.declarationDate.replace(/-/g, ''), 10);
+          const numB = parseInt(b.declarationDate.replace(/-/g, ''), 10);
+          return numB - numA; // Trie du plus récent au plus ancien
+        });
+        break;
+      case "byInterventionDate":
+        sortedInterventions = [...interventionsList].sort((a, b) => {
+          // Utilise localeCompare pour trier les chaînes de caractères de dates
+          return b.interventionDate.localeCompare(a.interventionDate);
+        });
+        break;
+      default:
+        sortedInterventions = [...interventionsList];
+    }
+    setSortedInterventionsList(sortedInterventions);
+  }
+}, [sortMethod, interventionsList]); // Dépendances du useEffect
     
     return (
       <div id="interventionsLister">
         {'counter' in props.user && <p>Le solde actuel de votre compteur est de : <b>{props.user.counter.toFixed(2)} points</b></p>}
-        {interventionsList && interventionsList[0] ? <ul className={"scrollable"}>
-          {interventionsList && interventionsList.map((intervention, index) => 
+        <select onChange={(e) => setSortMethod(e.target.value as "byInterventionDate" | "byDeclarationDate")}>
+          <option value="byDeclarationDate">Trier par date de déclaration</option>
+          <option value="byInterventionDate">Trier par date d'intervention</option>
+        </select>
+        {sortedInterventionsList && sortedInterventionsList.length > 0 ? <ul className={"scrollable"}>
+          {sortedInterventionsList.map((intervention, index) => 
           (<InterventionCard key={index} intervention={intervention} role={"member"} user={props.user}/>))}
         </ul> : 
         <p>Vous n'avez pas encore d'opérations déclarées au sein de la guilde {props.user?.guild}</p>}
