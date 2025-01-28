40f8f3ef2d6f09118c32e63ccdbae257c4cc62cc
MajorDown <romain.fouillaron@gmx.fr>
2025-01-28 21:52:53 +0100

    implémentation de api/guild avec authentifier

--- app/api/guild/[guild]/route.ts
+++ app/api/guild/[guild]/route.ts
@@ -1,22 +1,31 @@
 import { NextResponse } from "next/server";
 import databaseConnecter from "@/tools/api/databaseConnecter";
-import { tokenChecker } from "@/tools/api/tokenManager";
-import { UserMail, UserStatus } from "@/types";
+import { UserStatus } from "@/types";
 import MemberModel from "@/tools/api/models/model.member";
+import authentifier from "@/tools/api/authentifier";
 
 export async function GET(request: Request, { params }: { params: { guild: string } }) {
   console.log(`api/guild/[guild] ~> Requète de récupération des membres de la guilde ${params.guild}`);
   try {
     await databaseConnecter();
-    // AUTHENTIFICATION DE L'ADMIN
+    // VERIFICATION DE LA PRESENCE DES DONNEES
     const auth = request.headers.get('Authorization');
     const userToken = auth && auth.split(' ')[1];
     const userMail = request.headers.get('X-user-Email');
     const role = request.headers.get('X-role') as UserStatus;
-    const isAuthentified = userToken ? await tokenChecker(role, userToken, userMail as UserMail) : false;
-    if (!isAuthentified) {
+    if (!userMail || !role || !userToken) {
         console.log(`api/guild/[guild] ~> echec de l'authentification de l'utilisateur ${userMail}`);
         return NextResponse.json("Non autorisé", { status: 401 });
+    }
+    // VERIFICATION DE L'AUTHENTIFICATION
+    const authResponse = await authentifier({
+      model: role,
+      token: userToken,
+      userMail: userMail
+    });
+    if (!authResponse.authorized) {
+      console.log(`api/guild/[guild] ~> ${authResponse.error}`);
+      return NextResponse.json(authResponse.error, { status: 401 });
     }
     // RECUPERATION DES MEMBRES
     let users = await MemberModel.find({guild: params.guild}).lean();
