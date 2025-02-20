a2f0583a27d2b0615b59c2d16fef557a5f3df06e
MajorDown <romain.fouillaron@gmx.fr>
2025-02-20 23:57:22 +0100

    création du RuleManager, en attente de création de la RuleCard

--- components/GuildRulesManager.tsx
+++ components/GuildRulesManager.tsx
@@ -1,32 +1,30 @@
 import getGuildConfig from "@/tools/front/getGuildConfig";
-import { ConnectedMember, GuildConfig, GuildRules } from "@/types";
+import { ConnectedAdmin, GuildConfig, GuildRules } from "@/types";
 import { useState, useEffect } from "react";
 
-type GuildRulesViewerProps = {
-    member: ConnectedMember;
+type GuildRulesManagerProps = {
+    admin: ConnectedAdmin;
 }
 
-const GuildRulesViewer = (props: GuildRulesViewerProps) => {
+const GuildRulesManager = (props: GuildRulesManagerProps) => {
     const [rules, setRules] = useState<GuildRules>([]);
 
     useEffect(() => {
         const getRules = async () => {
-            const response = await getGuildConfig(props.member) as GuildConfig | Error;
+            const response = await getGuildConfig(props.admin) as GuildConfig | Error;
             if (response instanceof Error) {
                 setRules(["Erreur lors de la récupération des données de la guilde"]);
             }
             else setRules(response.rules || ["Aucun règlement n'a été défini pour cette guilde"])
         }
         getRules();
-    }, [props.member]);
+    }, [props.admin]);
 
     return (
-        <div id={"guildRulesViewer"}>
-            <ul>
-                {rules && rules.map((rule, index) => <li key={index}>{rule}</li>)}
-            </ul>
+        <div id={"guildRulesManager"}>
+            <p>guildRulesManager</p>
         </div>
     )
 }
 
-export default GuildRulesViewer;
+export default GuildRulesManager;
