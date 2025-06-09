2851bb84b6e3c1b0a2664023259c511deb969c20
MajorDown <romain.fouillaron@gmx.fr>
2025-06-09 14:24:47 +0200

    création du container de la 3 eme section

--- components/landing/sections/ThirdSection.tsx
+++ components/landing/sections/ThirdSection.tsx
@@ -1,3 +1,4 @@
+
 import SectionTitle from "../UI/SectionTitle";
 import GreenSection from "./GreenSection";
 import Style from "@/styles/components/landing/GreenSection.module.css";
@@ -11,6 +12,20 @@ const ThirdSection = (): JSX.Element => {
             <div id={Style.titleWrapper}>
                 <SectionTitle title={"Nos avantages"} />
                 <p id={Style.subTitle}>Pourquoi <span>utiliser</span> Agriguilder ?</p>
+                <div className={Style.sectionContainer}>
+                    <div id={Style.first}>
+                        <h3>Un accès <span>Personnalisé</span></h3>
+                        <p>Chaque adhérent possède un accès personnalisé via un site internet</p>
+                    </div>
+                    <div id={Style.second}>
+                        <h3>Déclaration <span>instantanée</span></h3>
+                        <p>Echange de temps et d'interventions réalisés entre membres</p>
+                    </div>
+                    <div id={Style.third}>
+                        <h3>Mises à jour <span>automatiques</span></h3>
+                        <p>Historique des échanges et des soldes de points</p>
+                    </div>
+                </div>
             </div>
         </GreenSection>
     );
